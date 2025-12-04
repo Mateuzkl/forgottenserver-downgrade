d370217c2561106e2c805dec3ed38a7eb0147c4f
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2025-12-04 18:06:57 -0300

    Move XML to CreatureScripts Revscripts

--- data/scripts/creaturescripts/player_login_logout.lua
+++ data/scripts/creaturescripts/player_login_logout.lua
@@ -1,32 +1,52 @@
 local loginMessage = CreatureEvent("loginMessage")
 
 function loginMessage.onLogin(player)
-	print(string.format("\27[32m%s%s\27[0m", player:getName(),
-	                       " has logged in."))
+    print(string.format("\27[32m%s has logged in.\27[0m", player:getName()))
 
-	-- Notify player about the rewards in their reward chest
-	local rewardChest = player:getRewardChest()
-	local rewardContainerCount = 0
-	for _, item in ipairs(rewardChest:getItems()) do
-		if item:getId() == ITEM_REWARD_CONTAINER then
-			rewardContainerCount = rewardContainerCount + 1
-		end
-	end
-	if rewardContainerCount > 0 then
-		player:sendTextMessage(MESSAGE_STATUS_DEFAULT, string.format("You have %d reward%s in your reward chest.", rewardContainerCount, rewardContainerCount > 1 and "s" or ""))
-	end
+    local rewardChest = player:getRewardChest()
+    local rewardContainerCount = 0
+    for _, item in ipairs(rewardChest:getItems()) do
+        if item:getId() == ITEM_REWARD_CONTAINER then
+            rewardContainerCount = rewardContainerCount + 1
+        end
+    end
+    if rewardContainerCount > 0 then
+        player:sendTextMessage(MESSAGE_STATUS_DEFAULT, string.format("You have %d reward%s in your reward chest.", rewardContainerCount, rewardContainerCount > 1 and "s" or ""))
+    end
 
-	return true
+    local serverName = configManager.getString(configKeys.SERVER_NAME)
+    local loginStr = "Welcome to " .. serverName .. "!"
+    if player:getLastLoginSaved() <= 0 then
+        loginStr = loginStr .. " Please choose your outfit."
+        player:sendOutfitWindow()
+    else
+        loginStr = string.format("Your last visit in %s: %s.", serverName, os.date("%d %b %Y %X", player:getLastLoginSaved()))
+    end
+    player:sendTextMessage(MESSAGE_STATUS_DEFAULT, loginStr)
+
+    local vocation = player:getVocation()
+    local promotion = vocation:getPromotion()
+    if player:isPremium() then
+        local value = player:getStorageValue(PlayerStorageKeys.promotion)
+        if value and value == 1 then
+            player:setVocation(promotion)
+        end
+    elseif not promotion then
+        player:setVocation(vocation:getDemotion())
+    end
+
+    player:registerEvent("logoutMessage")
+
+    player:updateStamina()
+    return true
 end
-
 loginMessage:register()
 
 local logoutMessage = CreatureEvent("logoutMessage")
-
 function logoutMessage.onLogout(player)
-	print(string.format("\27[31m%s%s\27[0m", player:getName(),
-	                       " has logged out."))
-	return true
+    print(string.format("\27[31m%s has logged out.\27[0m", player:getName()))
+    local playerId = player:getId()
+    nextUseStaminaTime[playerId] = nil
+    return true
 end
-
 logoutMessage:register()
