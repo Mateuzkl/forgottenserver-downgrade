5dc81001109f7d408c9424e3e2d23de3cba20a26
Skyyzyy <mcl_d2@hotmail.com>
2025-12-27 10:33:46 -0300

    [BUGFIX] Incorrectly displaying special skills.
    Refrence: https://github.com/otland/forgottenserver/issues/4612
    
    Refrence: https://github.com/otland/forgottenserver/pull/4613/commits/30cc3e82c283ac0f3e4fa52ba8134bc99ba07055

--- src/protocolgame.cpp
+++ src/protocolgame.cpp
@@ -2618,7 +2618,7 @@ void ProtocolGame::AddPlayerSkills(NetworkMessage& msg)
 		}
 
 		for (uint8_t i = SPECIALSKILL_FIRST; i <= SPECIALSKILL_LAST; ++i) {
-			msg.add<uint16_t>(static_cast<uint16_t>(std::min<int32_t>(100, player->varSpecialSkills[i])));
+			msg.add<uint16_t>(static_cast<uint16_t>(std::min<int32_t>(10000, player->varSpecialSkills[i])));
 			msg.add<uint16_t>(0);
 		}
 	}
