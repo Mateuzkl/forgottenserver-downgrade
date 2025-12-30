c11c82e76e69a85aed34ec87a65f44aefbf113bb
Skyyzyy <mcl_d2@hotmail.com>
2025-12-30 09:50:16 -0300

    [BUGFIX] Make it so that 1% resistance correctly reduces damage taken by 1.
    Reference: https://github.com/otland/forgottenserver/pull/4733/commits/57364f7130e12e6ab645008bbad0ce8bbadd9368

--- src/player.cpp
+++ src/player.cpp
@@ -1899,7 +1899,7 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 
 			const int16_t& absorbPercent = it.abilities->absorbPercent[combatTypeToIndex(combatType)];
 			if (absorbPercent != 0) {
-				damage -= std::round(damage * (absorbPercent / 100.));
+				damage -= std::ceil(damage * (absorbPercent / 100.));
 
 				uint16_t charges = item->getCharges();
 				if (charges != 0) {
@@ -1912,7 +1912,7 @@ BlockType_t Player::blockHit(Creature* attacker, CombatType_t combatType, int32_
 			if (field) {
 				const int16_t& fieldAbsorbPercent = it.abilities->fieldAbsorbPercent[combatTypeToIndex(combatType)];
 				if (fieldAbsorbPercent != 0) {
-					damage -= std::round(damage * (fieldAbsorbPercent / 100.));
+					damage -= std::ceil(damage * (fieldAbsorbPercent / 100.));
 
 					uint16_t charges = item->getCharges();
 					if (charges != 0) {
