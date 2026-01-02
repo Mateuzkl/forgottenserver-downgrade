c5aaa654ae483f512379bc3724c192e42de8494e
Skyyzyy <mcl_d2@hotmail.com>
2026-01-01 21:32:31 -0300

    Refactor(tile): Move getTile() into Tile class and remove unnecessary dynamic_cast from Thing.
    Reference: https://github.com/otland/forgottenserver/commit/292e3074f1998c5fa95b6891ebabdafc853a3a95

--- src/thing.h
+++ src/thing.h
@@ -30,15 +30,14 @@ public:
 		//
 	}
 
-	virtual Tile* getTile();
-	virtual const Tile* getTile() const;
-
 	virtual const Position& getPosition() const;
 	virtual int32_t getThrowRange() const = 0;
 	virtual bool isPushable() const = 0;
 
 	virtual Container* getContainer() { return nullptr; }
 	virtual const Container* getContainer() const { return nullptr; }
+	virtual Tile* getTile() { return nullptr; }
+	virtual const Tile* getTile() const { return nullptr; }
 	virtual Item* getItem() { return nullptr; }
 	virtual const Item* getItem() const { return nullptr; }
 	virtual Creature* getCreature() { return nullptr; }
