6545fabd9d5d3643be8f1fc637e69296707f3012
Skyyzyy <mcl_d2@hotmail.com>
2026-01-03 05:26:51 -0300

    Avoid duplicate getMountByID calls in tameMount and untameMount.
    Reference: https://github.com/otland/forgottenserver/commit/7ca99c82b4493f7029ee4b3bd532e439c63eb345

--- src/player.cpp
+++ src/player.cpp
@@ -4196,12 +4196,8 @@ bool Player::toggleMount(bool mount)
 
 bool Player::tameMount(uint16_t mountId)
 {
-	if (!g_game.mounts.getMountByID(mountId)) {
-		return false;
-	}
-
 	Mount* mount = g_game.mounts.getMountByID(mountId);
-	if (hasMount(mount)) {
+	if (!mount || hasMount(mount)) {
 		return false;
 	}
 
@@ -4211,12 +4207,8 @@ bool Player::tameMount(uint16_t mountId)
 
 bool Player::untameMount(uint16_t mountId)
 {
-	if (!g_game.mounts.getMountByID(mountId)) {
-		return false;
-	}
-
 	Mount* mount = g_game.mounts.getMountByID(mountId);
-	if (!hasMount(mount)) {
+	if (!mount || hasMount(mount)) {
 		return false;
 	}
 
