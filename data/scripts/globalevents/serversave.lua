7b20e6456c5daded2f2e92e2e4357b1c127e0c13
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-01-24 00:20:41 -0300

    Fix decimal formatting in server save warning

--- data/scripts/globalevents/serversave.lua
+++ data/scripts/globalevents/serversave.lua
@@ -1,51 +1,68 @@
 local config = {
-    time = "09:55:00"
+    time = "09:55:00"
 }
 
+
 local function ServerSave()
-    if configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN) then
-        Game.setGameState(GAME_STATE_SHUTDOWN)
-    else
-        local closeAtServerSave = configManager.getBoolean(configKeys.SERVER_SAVE_CLOSE)
-        if closeAtServerSave then
-            Game.setGameState(GAME_STATE_CLOSED)
-        end
+    if configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN) then
+        Game.setGameState(GAME_STATE_SHUTDOWN)
+    else
+        local closeAtServerSave = configManager.getBoolean(configKeys.SERVER_SAVE_CLOSE)
+        if closeAtServerSave then
+            Game.setGameState(GAME_STATE_CLOSED)
+        end
 
-        saveServer()
 
-        if configManager.getBoolean(configKeys.SERVER_SAVE_CLEAN_MAP) then
-            cleanMap()
-        end
+        saveServer()
 
-        if closeAtServerSave then
-            Game.setGameState(GAME_STATE_NORMAL)
-        end
-    end
+
+        if configManager.getBoolean(configKeys.SERVER_SAVE_CLEAN_MAP) then
+            cleanMap()
+        end
+
+
+        if closeAtServerSave then
+            Game.setGameState(GAME_STATE_NORMAL)
+        end
+    end
 end
+
 
 local function ServerSaveWarning(time)
-    local remainingTime = tonumber(time) - 60000
+    local remainingTime = tonumber(time) - 60000
 
-    if configManager.getBoolean(configKeys.SERVER_SAVE_NOTIFY_MESSAGE) then
-        Game.broadcastMessage("Server is saving game in " .. (remainingTime / 60000) .. " minute(s). Please logout.", MESSAGE_STATUS_WARNING)
-    end
 
-    if remainingTime > 60000 then
-        addEvent(ServerSaveWarning, 60000, remainingTime)
-    else
-        addEvent(ServerSave, 60000)
-    end
+    if configManager.getBoolean(configKeys.SERVER_SAVE_NOTIFY_MESSAGE) then
+        local message = "Server is saving game in " .. math.floor(remainingTime / 60000) .. " minute(s). Please logout."
+        if configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN) then
+            message = "Server is shutting down in " .. math.floor(remainingTime / 60000) .. " minute(s). Please logout."
+        end
+        Game.broadcastMessage(message, MESSAGE_STATUS_WARNING)
+    end
+
+
+    if remainingTime > 60000 then
+        addEvent(ServerSaveWarning, 60000, remainingTime)
+    else
+        addEvent(ServerSave, 60000)
+    end
 end
+
 
 local serverSave = GlobalEvent("ServerSave")
 function serverSave.onTime(interval)
-    local remainingTime = configManager.getNumber(configKeys.SERVER_SAVE_NOTIFY_DURATION) * 60000
-    if configManager.getBoolean(configKeys.SERVER_SAVE_NOTIFY_MESSAGE) then
-        Game.broadcastMessage("Server is saving game in " .. (remainingTime / 60000) .. " minute(s). Please logout.", MESSAGE_STATUS_WARNING)
-    end
+    local remainingTime = configManager.getNumber(configKeys.SERVER_SAVE_NOTIFY_DURATION) * 60000
+    if configManager.getBoolean(configKeys.SERVER_SAVE_NOTIFY_MESSAGE) then
+        local message = "Server is saving game in " .. math.floor(remainingTime / 60000) .. " minute(s). Please logout."
+        if configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN) then
+            message = "Server is shutting down in " .. math.floor(remainingTime / 60000) .. " minute(s). Please logout."
+        end
+        Game.broadcastMessage(message, MESSAGE_STATUS_WARNING)
+    end
 
-    addEvent(ServerSaveWarning, 60000, remainingTime)
-    return not configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN)
+
+    addEvent(ServerSaveWarning, 60000, remainingTime)
+    return not configManager.getBoolean(configKeys.SERVER_SAVE_SHUTDOWN)
 end
 serverSave:time(config.time)
 serverSave:register()
